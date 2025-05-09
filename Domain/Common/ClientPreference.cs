b10377b9facf82c2ec7078f1d3bdba288e9ab96f
mebrahimian <ebrahimian_4585@hotmail.com>
2025-05-10 01:34:57 +0330

    Add Preference (RTL,IsDark, Lang, theme, drawer)

---
+++ Domain/Common/ClientPreference.cs
@@ -0,0 +1,12 @@
+﻿namespace Erp100Af.Domain.Common
+{
+    public class ClientPreference
+    {
+        public string LanguageCode { get; set; } = "en-US";
+        public bool IsDarkMode { get; set; } = false;
+        public bool RightToLeft { get; set; } = false;
+        public bool DrawerOpen { get; set; } = true;
+        public string Theme { get; set; } = "BlazorErpTheme";
+    }
+
+}
